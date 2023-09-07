7176df68c319d17df86c163ea4e340183125ea94
Evan Joseph <evandromariano49@gmail.com>
2023-09-07 15:23:44 -0300

    feat: desacoplando components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>Evandro</h1>
-    <router-outlet></router-outlet>
+    <app-card></app-card>
     <hr />
+    <router-outlet></router-outlet>
   `,
   styleUrls: ['./app.component.css'],
 })
