fff358afad1d633401bb4f6095e1b4a2d1839d1f
Evan Joseph <evandromariano49@gmail.com>
2023-09-07 14:32:41 -0300

    feat: card-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Evandro</h1>
+    <router-outlet></router-outlet>
+    <hr />
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-trabalhando-com-services';
